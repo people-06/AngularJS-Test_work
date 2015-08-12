09e4b4af6760967532eb33570f8ab473f2fc4442
Lika Nikanorova <people-06@ya.ru>
2015-08-12 23:02:09 +0300

    Form Validation.
    Implement form validation. Have problem with bind or file-input. Need
    to be reviewed.

--- src/js/app.js
+++ src/js/app.js
@@ -115,3 +115,59 @@ CommissionApp.controller('CloseTicketViewCtrl', function ($scope, $modalInstance
         $modalInstance.dismiss('cancel');
     };
 });
+
+// Form validation
+
+CommissionApp.controller('validateCtrl', function($scope) {
+    $scope.namePattern = /([А-яЁё])+/;
+    $scope.emailPattern = /^([a-zA-Z0-9])+([a-zA-Z0-9._%+-])+@([a-zA-Z0-9_.-])+\.(([a-zA-Z]){2,6})$/;
+    $scope.phonePattern = /^[+\(\)0-9-\s]{5,18}$/;
+    $scope.filePattern = /^([a-zA-Z0-9А-яЁё\s_\\.\-:])+\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF|doc|DOC|pdf|PDF)$/;
+});
+
+
+// File validation
+CommissionApp.directive('validFile', function () {
+
+    return {
+        require: 'ngModel',
+        scope: {
+            fileTypes: '=valFileTypes',
+            fileSize: '=valFileSize'
+        },
+        link: function (scope, elem, attrs, ngModel) {
+
+            elem.bind('change', function () {
+                validate(elem[0].files);
+            });
+
+            function validate(files) {
+                var validType = true;
+                var validSize = true;
+                var filePattern = new RegExp("^([a-zA-Z0-9А-яЁё\s_\\.\-:])+\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF|doc|DOC|pdf|PDF)$");
+
+                // Check file type and size of each file
+                if (!filePattern.test(files[0].name)) {
+                    validType = false;
+                    //clearFileInput(elem[0]);
+                }
+                if( files[0] && files[0].size > 10485760 ) {
+                    validSize = false;
+                    //clearFileInput(elem[0]);
+                }
+
+                ngModel.$setValidity('valFileTypes', validType);
+                ngModel.$setValidity('valFileSize', validSize);
+            }
+        }
+    };
+
+    function clearFileInput(ctrl) {
+        try {
+            ctrl.value = null;
+        } catch(ex) { }
+        if (ctrl.value) {
+            ctrl.parentNode.replaceChild(ctrl.cloneNode(true), ctrl);
+        }
+    }
+});
